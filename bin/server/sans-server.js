120f83b42a7ec6d3ec2260111547e957a5a7552a
James Speirs <james.speirs@gmail.com>
2016-12-14 11:48:10 -0700

    set middleware logger name to use underscores

--- bin/server/sans-server.js
+++ bin/server/sans-server.js
@@ -248,7 +248,7 @@ SansServer.prototype.use = function(middleware) {
         if (typeof mw !== 'function') throw Error('Invalid middleware specified. Expected a function. Received: ' + mw);
 
         const name = mw.name || 'middleware-' + (middlewares.length + 1);
-        const logger = Log.firer(name);
+        const logger = Log.firer(name.replace(/([A-Z])/g, function($1){return "_" + $1}));
         const wrapped = function(req, res, next) {
             server.log = function(title, message, details) {
                 logger(req, title, message, details);
