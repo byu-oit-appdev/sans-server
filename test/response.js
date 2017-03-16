bdacb230ee913e84c4836d4b7a2be9b8f89c5e2a
James <james.speirs@gmail.com>
2017-03-16 15:14:30 -0600

    update for new request signature

--- test/response.js
+++ test/response.js
@@ -26,237 +26,161 @@ describe('response', () => {
         req = Request({});
     });
 
-    it('can clear a cookie', (done) => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.rawHeaders).to.equal('Set-Cookie: foo=; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
-                expect(res.cookies[0].serialized).to.equal('foo=; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can clear a cookie', () => {
+        const res = Response(req);
         res.clearCookie('foo');
         res.send();
+        return req.promise.then(res => {
+            expect(res.rawHeaders).to.equal('Set-Cookie: foo=; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
+            expect(res.cookies[0].serialized).to.equal('foo=; Expires=Thu, 01 Jan 1970 00:00:00 GMT');
+        });
     });
 
-    it('can set a cookie', (done) => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.rawHeaders).to.equal('Set-Cookie: foo=bar');
-                expect(res.cookies[0].serialized).to.equal('foo=bar');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can set a cookie', () => {
+        const res = Response(req);
         res.cookie('foo', 'bar');
         res.send();
+        return req.promise.then(res => {
+            expect(res.rawHeaders).to.equal('Set-Cookie: foo=bar');
+            expect(res.cookies[0].serialized).to.equal('foo=bar');
+        });
     });
 
-    it('cookie with same name creates second entry', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.rawHeaders).to.equal('Set-Cookie: foo=bar\nSet-Cookie: foo=baz');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('cookie with same name creates second entry', () => {
+        const res = Response(req);
         res.cookie('foo', 'bar');
         res.cookie('foo', 'baz');
         res.send();
+        return req.promise.then(res => {
+            expect(res.rawHeaders).to.equal('Set-Cookie: foo=bar\nSet-Cookie: foo=baz');
+        });
     });
 
-    it('can redirect', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.rawHeaders).to.equal('Location: http://foo.com');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can redirect', () => {
+        const res = Response(req);
         res.redirect('http://foo.com');
+        return req.promise.then(res => {
+            expect(res.rawHeaders).to.equal('Location: http://foo.com');
+        });
     });
 
     it('can detect if sent', () => {
-        const res = Response(req, function(err, res) {});
+        const res = Response(req);
         expect(res.sent).to.equal(false);
         res.send();
         expect(res.sent).to.equal(true);
     });
 
-    it('can send body only', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.statusCode).to.equal(200);
-                expect(res.body).to.equal('body');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can send body only', () => {
+        const res = Response(req);
         res.send('body');
+        return req.promise.then(res => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.equal('body');
+        });
     });
 
-    it('can send code and body', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.statusCode).to.equal(100);
-                expect(res.body).to.equal('body');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can send code and body', () => {
+        const res = Response(req);
         res.send(100, 'body');
+        return req.promise.then(res => {
+            expect(res.statusCode).to.equal(100);
+            expect(res.body).to.equal('body');
+        });
     });
 
-    it('can send body and headers', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.statusCode).to.equal(200);
-                expect(res.body).to.equal('body');
-                expect(res.rawHeaders).to.equal('foo: bar');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can send body and headers', () => {
+        const res = Response(req);
         res.send('body', { foo: 'bar' });
+        return req.promise.then(res => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.equal('body');
+            expect(res.rawHeaders).to.equal('foo: bar');
+        });
     });
 
-    it('can send code, body, and headers', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.statusCode).to.equal(100);
-                expect(res.body).to.equal('body');
-                expect(res.rawHeaders).to.equal('foo: bar');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can send code, body, and headers', () => {
+        const res = Response(req);
         res.send(100, 'body', { foo: 'bar' });
+        return req.promise.then(res => {
+            expect(res.statusCode).to.equal(100);
+            expect(res.body).to.equal('body');
+            expect(res.rawHeaders).to.equal('foo: bar');
+        });
     });
 
-    it('can send error for body', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(err).to.be.instanceOf(Error);
-                expect(res.statusCode).to.equal(500);
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can send error for body', () => {
+        const res = Response(req);
         res.send(Error('oops'));
+        return req.promise.then(res => {
+            expect(res.error).to.be.instanceOf(Error);
+            expect(res.statusCode).to.equal(500);
+        });
     });
 
-    it('error for body resets headers', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.headers).to.not.haveOwnProperty('Foo');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('error for body resets headers', () => {
+        const res = Response(req);
         res.set('foo', 'bar');
         res.send(Error('oops'));
+        return req.promise.then(res => {
+            expect(res.headers).to.not.haveOwnProperty('foo');
+        });
     });
 
-    it('can send object for body', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.body).to.equal(JSON.stringify({ foo: 'bar' }));
-                expect(res.headers['Content-Type']).to.equal('application/json');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can send object for body', () => {
+        const res = Response(req);
         res.send({ foo: 'bar' });
+        return req.promise.then(res => {
+            expect(res.body).to.equal(JSON.stringify({ foo: 'bar' }));
+            expect(res.headers['Content-Type']).to.equal('application/json');
+        });
     });
 
-    it('can use send status', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.body).to.equal('Not Found');
-                expect(res.statusCode).to.equal(404);
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can use send status', () => {
+        const res = Response(req);
         res.sendStatus(404);
+        return req.promise.then(res => {
+            expect(res.body).to.equal('Not Found');
+            expect(res.statusCode).to.equal(404);
+        });
     });
 
-    it('can set header', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.rawHeaders).to.equal('foo: bar');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can set header', () => {
+        const res = Response(req);
         res.set('foo', 'bar');
         res.send();
+        return req.promise.then(res => {
+            expect(res.rawHeaders).to.equal('foo: bar');
+        });
     });
 
-    it('can overwrite header', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.rawHeaders).to.equal('foo: baz');
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can overwrite header', () => {
+        const res = Response(req);
         res.set('foo', 'bar');
         res.set('foo', 'baz');
         res.send();
+        return req.promise.then(res => {
+            expect(res.rawHeaders).to.equal('foo: baz');
+        });
     });
 
-    it('can clear header', done => {
-        const res = Response(req, function(err, res) {
-            expect(res.headers).not.to.haveOwnProperty('foo');
-            done();
-        });
-
+    it('can clear header', () => {
+        const res = Response(req);
         res.set('foo', 'bar');
         res.clearHeader('foo');
         res.send();
+        return req.promise.then(res => {
+            expect(res.headers).not.to.haveOwnProperty('foo');
+        });
     });
 
-    it('can set status', done => {
-        const res = Response(req, function(err, res) {
-            try {
-                expect(res.statusCode).to.equal(100);
-                done();
-            } catch (e) {
-                done(e);
-            }
-        });
-
+    it('can set status', () => {
+        const res = Response(req);
         res.status(100);
         res.send();
+        return req.promise.then(res => {
+            expect(res.statusCode).to.equal(100);
+        });
     });
 
     it('can generate generic error response', () => {
@@ -266,8 +190,7 @@ describe('response', () => {
     });
 
     it('can get current state', () => {
-        const res = Response(req, () => {});
-
+        const res = Response(req);
         res.status(100);
         res.set('foo', 'bar');
         const state = res.state;
@@ -275,47 +198,44 @@ describe('response', () => {
         expect(state.headers.foo).to.equal('bar');
     });
 
-    it('calls send hooks prior to completion', done => {
+    it('calls send hooks prior to completion', () => {
         let hooked = false;
-        const res = Response(req, () => {
-            expect(hooked).to.be.true;
-            done();
-        });
+        const res = Response(req);
         res.hook(function(state) {
             hooked = true;
             expect(this).to.equal(res);
             expect(state).to.deep.equal(res.state);
         });
         res.send();
+        return req.promise.then(res => {
+            expect(hooked).to.be.true;
+        });
     });
 
-    it('cannot perform send during hook', done => {
-        const res = Response(req, (err, res) => {
-            expect(err.code).to.equal('ESSENT');
-            done();
-        });
+    it('cannot perform send during hook', () => {
+        const res = Response(req);
         res.hook(function(state) {
             res.send('ok');
         });
         res.send();
+        return req.promise.then(res => {
+            expect(res.error.code).to.equal('ESSENT');
+        })
     });
 
-    it('can update body during hook', done => {
-        const res = Response(req, (err, res) => {
-            expect(res.body).to.equal('ok');
-            done();
-        });
+    it('can update body during hook', () => {
+        const res = Response(req);
         res.hook(function(state) {
             res.body('ok');
         });
         res.send('fail');
+        return req.promise.then(res => {
+            expect(res.body).to.equal('ok');
+        });
     });
 
-    it('hook can return promise', done => {
-        const res = Response(req, (err, res) => {
-            expect(res.body).to.equal('ok');
-            done();
-        });
+    it('hook can return promise', () => {
+        const res = Response(req);
         res.hook(function(state) {
             return Promise.resolve()
                 .then(function() {
@@ -323,13 +243,13 @@ describe('response', () => {
                 });
         });
         res.send('fail');
+        return req.promise.then(res => {
+            expect(res.body).to.equal('ok');
+        });
     });
 
-    it('hook can use callback', done => {
-        const res = Response(req, (err, res) => {
-            expect(res.body).to.equal('ok');
-            done();
-        });
+    it('hook can use callback', () => {
+        const res = Response(req);
         res.hook(function(state, callback) {
             setTimeout(function() {
                 res.body('ok');
@@ -337,5 +257,8 @@ describe('response', () => {
             }, 0);
         });
         res.send('fail');
+        return req.promise.then(res => {
+            expect(res.body).to.equal('ok');
+        });
     });
 });
