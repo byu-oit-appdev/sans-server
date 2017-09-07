f209f2032ce2665c63d27519f59925d483a0c9a7
James <james.speirs@gmail.com>
2017-09-07 16:22:29 -0600

    add query parameter parsing and building tests

--- test/request.js
+++ test/request.js
@@ -15,9 +15,9 @@
  *    limitations under the License.
  **/
 'use strict';
+const captureErrors = require('./capture-error');
 const expect        = require('chai').expect;
 const Request       = require('../bin/server/request');
-const schema        = require('../bin/server/schemas').request;
 const SansServer    = require('../bin/server/sans-server');
 
 process.on('unhandledRejection', err => console.error(err.stack));
@@ -28,6 +28,51 @@ describe('request', () => {
         const server = SansServer();
         const req = server.request();
         expect(req).to.be.instanceOf(Request);
+    });
+
+    describe('query', () => {
+        let server;
+
+        beforeEach(() => {
+            server = new SansServer();
+        });
+
+        it('from path', () => {
+            const err = captureErrors();
+            server.use((req, res, next) => {
+                expect(req.query).to.deep.equal({ a: '1', b: '', c: true, d: '4' });
+                expect(req.url).to.equal('?a=1&b=&c&d=4');
+                res.send();
+            });
+            return server.request('?a=1&b=&c&d=4')
+                .on('error', err.catch)
+                .then(() => err.report());
+        });
+
+        it('from query string', () => {
+            const err = captureErrors();
+            server.use((req, res, next) => {
+                expect(req.query).to.deep.equal({ a: '1', b: '', c: true, d: '4' });
+                expect(req.url).to.equal('?a=1&b=&c&d=4');
+                res.send();
+            });
+            return server.request({ query: 'a=1&b=&c&d=4' })
+                .on('error', err.catch)
+                .then(() => err.report());
+        });
+
+        it('from query', () => {
+            const err = captureErrors();
+            server.use((req, res, next) => {
+                expect(req.query).to.deep.equal({ a: '1', b: true, c: '', d: '4' });
+                expect(req.url).to.equal('?a=1&b&c=&d=4');
+                res.send();
+            });
+            return server.request({ query: { a: 1, b: true, c: '', d: '4' }})
+                .on('error', err.catch)
+                .then(() => err.report());
+        });
+
     });
 
     describe('middleware', () => {
@@ -88,154 +133,3 @@ describe('request', () => {
     });
 
 });
-/*
-
-describe('request 2', () => {
-
-    describe('schema', () => {
-
-        it('cannot be null', () => {
-            expect(() => schema.validate(null)).to.throw(Error);
-        });
-
-        it('can be undefined', () => {
-            expect(() => schema.validate()).to.throw(Error);
-        });
-
-        it('can be an empty object', () => {
-            expect(() => schema.validate({})).not.to.throw(Error);
-        });
-
-        describe('body', () => {
-
-            it('can be null', () => {
-                expect(() => schema.validate({ body: null })).not.to.throw(Error);
-            });
-
-            it('can be object', () => {
-                expect(() => schema.validate({ body: {} })).not.to.throw(Error);
-            });
-
-            it('can be string', () => {
-                expect(() => schema.validate({ body: '' })).not.to.throw(Error);
-            });
-
-            it('cannot be number', () => {
-                expect(() => schema.validate({ body: 1 })).to.throw(Error);
-            });
-
-        });
-
-        describe('headers', () => {
-
-            it('can be null', () => {
-                expect(() => schema.validate({ headers: null })).not.to.throw(Error);
-            });
-
-            it('null transforms to object', () => {
-                expect(schema.normalize({ headers: null }).headers).to.deep.equal({});
-            });
-
-            it('can be object', () => {
-                expect(() => schema.validate({ headers: {} })).not.to.throw(Error);
-            });
-
-            it('can be object with strings', () => {
-                expect(() => schema.validate({ headers: { foo: 'bar' } })).not.to.throw(Error);
-            });
-
-            it('can be object with numbers', () => {
-                expect(() => schema.validate({ headers: { foo: 1 } })).to.throw(Error);
-            });
-
-            it('cannot be number', () => {
-                expect(() => schema.validate({ headers: 1 })).to.throw(Error);
-            });
-
-        });
-
-        describe('path', () => {
-
-            it('adds start slash', () => {
-                const o = schema.normalize({ path: 'abc' });
-                expect(o.path).to.equal('/abc');
-            });
-
-            it('removes trailing slash', () => {
-                const o = schema.normalize({ path: '/abc/' });
-                expect(o.path).to.equal('/abc');
-            });
-
-        });
-
-        describe('query', () => {
-
-            it('can be null', () => {
-                expect(() => schema.validate({ query: null })).not.to.throw(Error);
-            });
-
-            it('can be object with strings', () => {
-                expect(() => schema.validate({ query: { foo: 'bar' } })).not.to.throw(Error);
-            });
-
-            it('can be object with empty array', () => {
-                expect(() => schema.validate({ query: { foo: [] } })).not.to.throw(Error);
-            });
-
-            it('can be object with array of string', () => {
-                expect(() => schema.normalize({ query: { foo: ['bar', 'baz'] } })).not.to.throw(Error);
-            });
-
-        });
-
-    });
-
-    describe('request instance', () => {
-
-        it('can accept undefined', () => {
-            expect(() => request()).not.to.throw(Error);
-        });
-
-        it('can provide path via string', () => {
-            const req = request('/foo/bar');
-            expect(req.path).to.equal('/foo/bar');
-        });
-
-        it('can pass query via path', () => {
-            const req = request({ path: '/abc?foo=bar&baz=123' });
-            expect(req.path).to.equal('/abc');
-            expect(Object.keys(req.query)).to.deep.equal(['foo', 'baz']);
-            expect(req.query.foo).to.equal('bar');
-            expect(req.query.baz).to.equal('123');
-        });
-
-        it('lowercases headers', () => {
-            const req = request({ headers: { Foo: 'bar' }});
-            expect(req.headers).to.have.ownProperty('foo');
-            expect(Object.keys(req.headers).length).to.equal(1);
-            expect(req.headers.foo).to.equal('bar');
-        });
-
-        it('can resolve', () => {
-            const req = request();
-            const o = {};
-            req.resolve(o);
-            return req.promise.then(v => {
-                expect(v).to.equal(o);
-            });
-        });
-
-        it('can reject', () => {
-            const req = request();
-            const o = {};
-            req.reject(o);
-            return req.promise
-                .then(
-                    v => { throw Error('Wrong error'); },
-                    e => { expect(e).to.equal(o); }
-                );
-        });
-
-    });
-
-});*/
