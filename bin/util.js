9fed28b4a280912e268f508b8757e640b0d22c2b
James <james.speirs@gmail.com>
2017-09-28 15:23:59 -0600

    remove log add seconds

--- bin/util.js
+++ bin/util.js
@@ -15,27 +15,7 @@
  *    limitations under the License.
  **/
 'use strict';
-
-/**
- * @interface LogEvent
- * @type {Object}
- * @property {string} action The sub-category that specifies the type of log message.
- * @property {string} category The category that the log falls within.
- * @property {object} details An object listing all the pertinent data that is associated with the log.
- * @property {string} message The log message.
- * @property {number} timestamp When the log event occurred.
- */
-
-/**
- * If an error occurs while creating or sending the response then this event is emitted with the error. The
- * error contains
- * the send function is called multiple times for a response.
- * @interface MetaError
- * @type {Error}
- * @property {string} code A code that specifies the error classification.
- * @property {string} message The error message.
- * @property {string} stack The error message stack.
- */
+const util      = require('util');
 
 /**
  * Copy a value.
@@ -45,6 +25,15 @@
 exports.copy = function(value) {
     const map = new WeakMap();
     return copy(value, map);
+};
+
+/**
+ * Call util.format with args.
+ * @param {object} args
+ * @returns {string}
+ */
+exports.format = function(args) {
+    return util.format.apply(util, args);
 };
 
 /**
@@ -62,37 +51,17 @@ exports.isPlainObject = function (o) {
     return !(!prototype || typeof prototype !== 'object' || !prototype.hasOwnProperty('isPrototypeOf'));
 };
 
-/**
- * Produce a log event.
- * @param {string} category
- * @param {object} args An arguments object with: [action], message, [details]
- * @returns {LogEvent}
- */
-exports.log = function(category, args) {
-    const event = {
-        action: 'log',
-        category: String(category),
-        details: {},
-        message: '',
-        timestamp: Date.now()
-    };
+exports.seconds = function seconds(milliseconds) {
+    let seconds = milliseconds / 1000;
 
-    // figure out what parameters were passed in on args
-    const length = args.length;
-    if (length === 0) {
-        const err = Error('Invalid number of log arguments. Expected at least one. Received: ' + length);
-        err.code = 'EPARM';
-        throw err;
-    } else if (length === 1 || (args.length === 2 && typeof args[1] === 'object')) {
-        event.message = String(args[0]);
-        if (args[1]) event.details = args[1];
-    } else {
-        event.action = String(args[0]);
-        event.message = String(args[1]);
-        if (args[2]) event.details = args[2];
-    }
+    if (seconds > 9999) return '9999+';
+    if (seconds > 999) return Math.round(seconds) + ' ';
 
-    return event;
+    const numeral = Math.floor(seconds);
+    const nLength = String(numeral).length;
+    const decimal = String(Math.round((seconds - numeral) * Math.pow(10, 4 - nLength)) / Math.pow(10, 4 - nLength)).split('.')[1];
+
+    return numeral + '.' + (!decimal ? '000' : decimal + '0'.repeat(3)).substr(0, 4 - nLength);
 };
 
 /**
